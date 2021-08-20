ae9385dbdeac5f8f3a5e9cdf5e0c48f48460e6e4
Anton Shirinkin <72990795+Ant0nS@users.noreply.github.com>
2021-08-20 17:09:47 +0300

    Add files via upload

---
+++ ITMO.Cpp.Practice13.Exercise/ITMO.Cpp.Practice13.Exercise/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include "Human.h"
+#include "Student.h"
+#include "Teacher.h"
+
+
+int main()
+{
+	system("chcp 1251");
+	// Оценкистудента
+	std::vector<int> scores;
+	// Добавление оценок студента в вектор
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(4);
+	scores.push_back(4);
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+	student* stud = new student("Петров", "Иван", "Алексеевич", scores);
+	std::cout << stud->get_full_name() << std::endl;
+	std::cout << "Средний балл : " << stud->get_data() << std::endl;
+
+	unsigned int teacher_work_time = 40;
+	teacher* tch = new teacher("Сергеев", "Дмитрий", "Сергеевич", teacher_work_time);
+	std::cout << tch->get_full_name() << std::endl;
+	std::cout << "Количество часов: " << tch->get_data() << std::endl;
+	return 0;
+}
